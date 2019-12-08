34b13349ef050b992341d014ea210fd85ca8e742
theonlyamos <theonlyamos@gmail.com>
2019-12-08 08:38:12 +0000

    fixed font load

--- resources/views/layouts/store_metronic.blade.php
+++ resources/views/layouts/store_metronic.blade.php
@@ -14,7 +14,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 		<!--begin::Fonts -->
-		<script src="https:/ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js')}}"></script>
+		<script src="https:/ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js"></script>
 		<script>
 			WebFont.load({
 				google: {
