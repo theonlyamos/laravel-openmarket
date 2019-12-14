d74aaeb8a2a75e415c11aabab00c726dcb585b12
theonlyamos <theonlyamos@gmail.com>
2019-12-14 18:24:42 +0000

    Store/AuthController

--- app/Http/Controllers/Store/AuthController.php
+++ app/Http/Controllers/Store/AuthController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use App\User;
 
-class AuthContoller extends Controller
+class AuthController extends Controller
 {
     //
 
