6d20cf8fe353b9d0059beb50442c6ede88241dd6
theonlyamos <theonlyamos@gmail.com>
2020-01-23 22:49:40 +0000

    product update request

--- app/Http/Controllers/Store/StoreController.php
+++ app/Http/Controllers/Store/StoreController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Store;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\storeProductsPost;
+use App\Http\Requests\storeProductUpdate;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
