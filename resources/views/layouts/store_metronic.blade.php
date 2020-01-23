6cfdd8a2400d608c34563933bbc3ce888fc56863
theonlyamos <theonlyamos@gmail.com>
2020-01-23 13:44:27 +0000

    store metronic footer copyright

--- resources/views/layouts/store_metronic.blade.php
+++ resources/views/layouts/store_metronic.blade.php
@@ -1045,10 +1045,7 @@
 					<!-- begin:: Footer -->
 					<div class="kt-footer kt-grid__item kt-grid kt-grid--desktop kt-grid--ver-desktop" id="kt_footer">
 						<div class="kt-footer__copyright">
-                            <script>
-                                document.write(new Date().getFullYear());
-                            </script>  &copy;  <a href="{{config('app.url')}}" target="_blank" class="kt-link">OpenMarket.</a>
-						</div>
+                            <script>document.write(new Date().getFullYear());</script> &copy; <a href="{{config('app.url')}}" target="_blank" class="kt-link">OpenMarket.</a>
 						</div>
 						<div class="kt-footer__menu">
 							<a href="http:/keenthemes.com/metronic" target="_blank" class="kt-footer__menu-link kt-link">About</a>
