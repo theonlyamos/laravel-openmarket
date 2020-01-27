aa5e2394a6f90d0dcb22201b527e6b0e55761571
theonlyamos <theonlyamos@gmail.com>
2020-01-27 20:23:27 +0000

    contact and about titles

--- app/Http/Controllers/Store/StoreController.php
+++ app/Http/Controllers/Store/StoreController.php
@@ -25,12 +25,12 @@ class StoreController extends Controller
 
     public function contact(){
         $site_info = DB::select('select * from site_info');
-        return view('contact', ['site' => $site_info[0]]);
+        return view('contact', ['title' => 'Contact Us', 'site' => $site_info[0]]);
     }
 
     public function about(){
         $site_info = DB::select('select * from site_info');
-        return view('about', ['site' => $site_info[0]]);
+        return view('about', ['title' => 'About Us', 'site' => $site_info[0]]);
     }
 
     public function products($store_id, Request $request){
