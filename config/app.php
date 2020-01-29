b19b9f8c3b0ed3101494cbba69271d792f8ee279
theonlyamos <theonlyamos@gmail.com>
2020-01-29 20:22:43 +0000

    commented out laravel/cashier in config/app

--- config/app.php
+++ config/app.php
@@ -165,7 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
-        Laravel\Cashier\CashierServiceProvider::class,
+         // Laravel\Cashier\CashierServiceProvider::class,
 
         /*
          * Application Service Providers...
