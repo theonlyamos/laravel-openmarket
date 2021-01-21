1c2165699e5d42568e4ae71c186e43eb5b7a2a0c
theonlyamos <theonlyamos@gmail.com>
2021-01-21 19:25:32 +0000

    added semi-colon in app/User:10

--- app/User.php
+++ app/User.php
@@ -5,7 +5,7 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Auth\Passwords\CanResetPassword
+use Illuminate\Auth\Passwords\CanResetPassword;
 
 class User extends Authenticatable
 {
