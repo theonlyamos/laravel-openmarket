43a24d9f559ce6f4b803709825f31a05a416eb29
theonlyamos <theonlyamos@gmail.com>
2021-02-22 20:43:00 +0000

    site info seeder 1

--- database/seeders/SiteInfoSeeder.php
+++ database/seeders/SiteInfoSeeder.php
@@ -21,6 +21,8 @@ class SiteInfoSeeder extends Seeder
             'box_office'      => 'P. O. Box MC 2185, Takoradi',
             'phone_developer' => '+233 557 821 030',
             'phone_info'      => '+233 557 821 030',
+            'phone_admin'     => '+233 249 577 827',
+            'phone_support'   => '+233 557 821 030',
             'email_developer' => 'theonlyamos@gmail.com',
             'email_info'      => 'info@openmartgh.com',
             'email_support'   => 'support@openmartgh.com',
